090b196e6ef2b14db77f0ff316ae468548049fc1
Ilyat9 <107850970+Ilyat9@users.noreply.github.com>
2025-09-27 08:52:01 +0300

    Update days_test.cpp

--- test/days_test.cpp
+++ test/days_test.cpp
@@ -2,44 +2,44 @@
 #include "count_days.h"
 
 TEST(CountDays_NormalCases, ExampleFromTask) {
-    int days = count_days(100, 10, 910);
+    int days = days(100, 10, 910);
     EXPECT_EQ(days, 10);
 }
 
 TEST(CountDays_NormalCases, ReachesInOneDay) {
-    EXPECT_EQ(count_days(10, 5, 7), 1);
-    EXPECT_EQ(count_days(20, 0, 15), 1);
+    EXPECT_EQ(days(10, 5, 7), 1);
+    EXPECT_EQ(days(20, 0, 15), 1);
 }
 
 TEST(CountDays_NormalCases, SeveralDaysNeeded) {
-    EXPECT_EQ(count_days(5, 2, 8), 2); 
-    EXPECT_EQ(count_days(3, 1, 7), 3);  
+    EXPECT_EQ(days(5, 2, 8), 2); 
+    EXPECT_EQ(days(3, 1, 7), 3);  
 }
 
 TEST(CountDays_ErrorCases, NegativeHeight) {
-    EXPECT_EQ(count_days(5, 2, 0), 0); 
-    EXPECT_EQ(count_days(5, 2, -10), -1);
+    EXPECT_EQ(days(5, 2, 0), 0); 
+    EXPECT_EQ(days(5, 2, -10), -1);
 }
 
 TEST(CountDays_ErrorCases, UpspeedLessOrEqualDownspeed) {
-    EXPECT_EQ(count_days(5, 5, 20), -1);
-    EXPECT_EQ(count_days(4, 6, 30), -1);
+    EXPECT_EQ(days(5, 5, 20), -1);
+    EXPECT_EQ(days(4, 6, 30), -1);
 }
 
 TEST(CountDays_ErrorCases, ZeroOrNegativeSpeeds) {
-    EXPECT_EQ(count_days(0, 0, 10), -1);
-    EXPECT_EQ(count_days(-3, 1, 10), -1);
-    EXPECT_EQ(count_days(3, -1, 10), -1); 
+    EXPECT_EQ(days(0, 0, 10), -1);
+    EXPECT_EQ(days(-3, 1, 10), -1);
+    EXPECT_EQ(days(3, -1, 10), -1); 
 }
 
 TEST(CountDays_Boundary, ZeroHeight) {
-    EXPECT_EQ(count_days(5, 2, 0), 0);
+    EXPECT_EQ(days(5, 2, 0), 0);
 }
 
 TEST(CountDays_Boundary, JustEnoughBeforeNight) {
-    EXPECT_EQ(count_days(10, 9, 10), 1);
+    EXPECT_EQ(days(10, 9, 10), 1);
 }
 
 TEST(CountDays_Boundary, LargeHeight) {
-    EXPECT_EQ(count_days(10, 1, 100), 11);
+    EXPECT_EQ(days(10, 1, 100), 11);
 }
