e0da788aaa96f9b7479d6c3415126d7d487b63aa
Ilyat9 <107850970+Ilyat9@users.noreply.github.com>
2025-09-27 08:23:46 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream> 
+#include "include/days.h"
+using namespace std;
+
+int main(){
+    int UpSpeed, DownSpeed, desiredHeight;
+    cin >> UpSpeed >> DownSpeed >> desiredHeight;
+    int days_passed = days(Upspeed, Downspeed, Desiredheight);
+    cout << days << endl;
+    return 0;
+}
